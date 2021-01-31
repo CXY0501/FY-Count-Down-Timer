2560eb4195d3f6ec5865902fa9fdb5a325c511c8
CXY0501 <chen.xy.2@foxmail.com>
2021-01-31 21:27:29 +0800

    2021013102

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
-   HOME PAGE
+  <div class="middle">
+    <span class="Month">
+     <div>Till Month End</div>
+    </span>
+    <span class="Quarter">
+     <div>Till Quarter End</div>
+    </span>
+    <span class="Year">
+     <div>Till Year End</div>
+    </span>
+  </div>
   </div>
 </template>
 
@@ -17,6 +27,24 @@ export default {
 </script>
 
 <style scoped>
-
+.middle{
+  position: fixed;
+  margin-top: 40vh;
+  margin-left: 12.5vw;
+  width: 75vw;
+  height: 20vh;
+}
+.middle span{
+  display: inline-block;
+  width: 25vw;
+  text-align: center;
+  color: red;
+}
+.middle div{
+  margin-left: 5vw;
+  width: 15vw;
+  border-radius: 10px;
+  background-color: black;
+}
 
 </style>
